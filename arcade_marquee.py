4ef3af738a6f94b321f4cb971a4eabaaa357ba72
cdeisler <c33jd13s3l@gmail.com>
2023-03-18 00:52:51 -0400

    continue rendering

--- arcade_marquee.py
+++ arcade_marquee.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QDesktopWidget
-from PyQt5.QtGui import QPixmap, QPainter
-
+from PyQt5.QtGui import QPixmap, QPainter, QBrush
+from PyQt5.QtCore import Qt
 class ArcadeMarquee(QWidget):
     def __init__(self):
         super().__init__()
@@ -14,24 +14,43 @@ class ArcadeMarquee(QWidget):
         self.setGeometry(screen)
         
         # Load the background image and set it as the window's background
-        bg_image = QPixmap('/home/kiosk/keogeo/marquees/neo-geo-marquee.jpg')
+        #bg_pixmap = QPixmap('/home/kiosk/keogeo/marquees/neo-geo-marquee.jpg')
+        # bg_pixmap = bg_pixmap.scaled(screen.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+
+        # brush = QBrush(bg_pixmap)
         self.setAutoFillBackground(True)
-        palette = self.palette()
-        palette.setBrush(self.backgroundRole(), bg_image)
-        self.setPalette(palette)
+        # palette = self.palette()
+        # palette.setBrush(self.backgroundRole(), brush)
+        # self.setPalette(palette)
         
-        # Create a painter to draw the images on top of the background
-        self.painter = QPainter(self)
+        # Load the background image
+        bg_image = QPixmap('/home/kiosk/keogeo/marquees/neo-geo-marquee.jpg')
+        bg_image = bg_image.scaled(screen.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
         
-        # Add the images to the painter
-        self.add_image('/home/kiosk/keogeo/marquees/aof.jpg', 100, 50)
-        self.add_image('/home/kiosk/keogeo/marquees/aof2.jpg', 200, 50)
-        self.add_image('/home/kiosk/keogeo/marquees/aof3.jpg', 300, 50)
-        
-        # Show the window on the second screen
-        self.move(screen.left(), screen.top())
-        self.show()
-        
+        bg_label = QLabel(self)
+        bg_label.setPixmap(bg_image)
+        bg_label.setGeometry(0, 0, screen.size().width(), screen.size().height())
+
+        # Load the three image overlays
+        overlay1_image = QPixmap('/home/kiosk/keogeo/marquees/aof.jpg')
+        overlay1_label = QLabel(self)
+        overlay1_label.setPixmap(overlay1_image)
+        overlay1_label.setGeometry(200, 200, 400, 488)
+
+        overlay2_image = QPixmap('/home/kiosk/keogeo/marquees/aof2.jpg')
+        overlay2_label = QLabel(self)
+        overlay2_label.setPixmap(overlay2_image)
+        overlay2_label.setGeometry(600, 200, 400, 488)
+
+        overlay3_image = QPixmap('/home/kiosk/keogeo/marquees/aof3.jpg')
+        overlay3_label = QLabel(self)
+        overlay3_label.setPixmap(overlay3_image)
+        overlay3_label.setGeometry(1000, 200, 400, 416)
+
+        self.showFullScreen()
+        #self.show()
+
     def add_image(self, filename, x, y):
         # Load the image file
         pixmap = QPixmap(filename)
